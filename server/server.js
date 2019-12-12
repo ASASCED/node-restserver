486e0ff2ee10103730c999ac62013aa01f5a8fea
Anix <jesus_heaven117@outlook.com>
2019-12-11 20:57:45 -0600

    Npm start listo

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 require('./config/config')
 
 const express = require('express')
-const app = express()
-
 const bodyParser = require('body-parser')
+const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
